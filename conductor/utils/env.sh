cb08b521694dce28b11c05f333832b5b3d16d2b7
Christopher T. Cannon <christophertcannon@gmail.com>
2011-11-22 15:11:35 -0500

    Added HUBO openRAVE model.

--- conductor/utils/env.sh
+++ conductor/utils/env.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-ACES_ROOT=$HOME/thesis/aces
+ACES_ROOT=$HOME/ccannon/hubo2/conductor
 
-source /opt/ros/cturtle/setup.sh
-#RTT_COMPONENT_PATH=/home/bob/thesis/aces/build:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+source /opt/ros/electric/setup.sh
 export ROS_PACKAGE_PATH=$ACES_ROOT/build:$ROS_PACKAGE_PATH
-export RTT_COMPONENT_PATH=/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
+export RTT_COMPONENT_PATH=/opt/ros/electric/stacks/orocos_toolchain_ros/rtt/install/lib/orocos
 
 export UTILS=$ACES_ROOT/utils
 export ACES_PATH=$ACES_ROOT/build/aces/bin:$UTILS
 export PATH=$ACES_PATH:$PATH
 
 #LD_LIBRARY_PATH=$ACES_ROOT/support/lib:/usr/local/webots/lib/:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/webots/lib/:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib:/opt/ros/cturtle/stacks/orocos_toolchain_ros/rtt/install/lib/orocos/plugins:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/webots/lib/:/opt/ros/electric/stacks/orocos_toolchain_ros/rtt/install/lib:/opt/ros/electric/stacks/orocos_toolchain_ros/rtt/install/lib/orocos/plugins:$LD_LIBRARY_PATH
 
 export WEBOTS_STDOUT=1
 export WEBOTS_STDERR=1
