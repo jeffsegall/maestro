772b9bbf97f40c6cabd426c608e8970a16f7baf2
Christopher T. Cannon <christophertcannon@gmail.com>
2011-11-29 16:32:33 -0500

    Moved around openrave models. Created files which hook up openrave to ROS.

---
+++ maestro/install-ros-diamondback-openrave.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install ros-diamondback-desktop-full ros-diamondback-orocos-toolchain-ros
+cd /opt/ros/diamondback/stacks
+sudo chown -R `whoami` *
+svn co https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/ .
+hg clone https://kforge.ros.org/armnavigation/armnavigation
+svn co https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_controllers/branches/pr2_controllers-1.4/pr2_controllers_msgs
+svn co https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_common/trunk/pr2_msgs
+rosdep install openrave_planning
