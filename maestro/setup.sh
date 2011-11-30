061029a1e8990e7e04a76525c9640496d825a992
Christopher T. Cannon <christophertcannon@gmail.com>
2011-11-30 10:22:17 -0500

    Clean up.

--- maestro/setup.sh
+++ maestro/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ln -s $HOME/hubo2/maestro/jaemi_hubo.launch.xml /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/jaemi_hubo.launch.xml
-ln -s $HOME/hubo2/maestro/models /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/models
+ln -sf $HOME/hubo2/maestro/launch/jaemi_hubo.launch.xml /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/jaemi_hubo.launch.xml
+ln -sf $HOME/hubo2/maestro/models /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/models
 
