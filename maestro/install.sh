f4a41fb4d4981027e9b1d91ca24c1793683e3758
Christopher T. Cannon <christophertcannon@gmail.com>
2011-12-05 13:51:15 -0500

    Script fixes based on Jeff's install.

--- maestro/install.sh
+++ maestro/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # This script installs ROS Diamondback with the Orocos Toolchain and
 # openRAVE stacks.
-sudo apt-get update
-sudo apt-get install ros-diamondback-desktop-full ros-diamondback-orocos-toolchain-ros
+apt-get update
+apt-get install ros-diamondback-desktop-full ros-diamondback-orocos-toolchain-ros
 cd /opt/ros/diamondback/stacks
-sudo chown -R `whoami` *
 svn co https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/ .
 hg clone https://kforge.ros.org/armnavigation/armnavigation
 svn co https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_controllers/branches/pr2_controllers-1.4/pr2_controllers_msgs
 svn co https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_common/trunk/pr2_msgs
+source /opt/ros/diamondback/setup.sh
 rosdep install openrave_planning
