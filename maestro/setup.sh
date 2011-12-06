e5a48901afcb0186c8e7f022b0614c90c418267c
Christopher T. Cannon <christophertcannon@gmail.com>
2011-12-06 11:51:36 -0500

    Added test python file.

--- maestro/setup.sh
+++ maestro/setup.sh
@@ -2,3 +2,4 @@
 source /opt/ros/diamondback/setup.sh
 ln -sf $HOME/hubo2/maestro/launch/jaemi_hubo.launch.xml /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/jaemi_hubo.launch.xml
 ln -sf $HOME/hubo2/maestro/models /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/models
+ln -sf $HOME/hubo2/maestro/src /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/maestro-src
---
+++ maestro/src/test.py
@@ -0,0 +1,6 @@
+from openravepy import *
+from numpy import *
+import time
+env = Environment()
+env.SetViewer('qtcoin')
+env.Load('openrave_robot_control/models/jaemi_hubo/jaemiHubo.robot.xml')
