8252526f379d61d6aa4d514ca38e967cdd8adb29
Jeff Segall <jeff@goldeneye.(none)>
2011-12-06 11:53:21 -0500

    Fixed paths in setup script

--- maestro/setup.sh
+++ maestro/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/diamondback/setup.sh
-ln -sf $HOME/hubo2/maestro/launch/jaemi_hubo.launch.xml /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/jaemi_hubo.launch.xml
-ln -sf $HOME/hubo2/maestro/models /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/models
+ln -sf `pwd`/launch/jaemi_hubo.launch.xml /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/jaemi_hubo.launch.xml
+ln -sf `pwd`/models /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/models
