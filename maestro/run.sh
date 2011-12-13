74ba65862a290c98786bcd7e4b8a3cd193c69456
Jeff Segall <jeff@goldeneye.(none)>
2011-12-13 14:38:46 -0500

    Listening for messages in test.py

--- maestro/run.sh
+++ maestro/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=/opt/ros/diamondback/stacks/openrave_planning/openrave/lib/python2.7/site-packages/:$PYTHONPATH
+export PYTHONPATH=/opt/ros/diamondback/ros/core/roslib/src:/opt/ros/diamondback/stacks/ros_comm/clients/rospy/src:/opt/ros/diamondback/stacks/openrave_planning/openrave/lib/python2.7/site-packages:$PYTHONPATH
 cp src/test.py /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/.
 roslaunch openrave_robot_control jaemi_hubo.launch.xml
