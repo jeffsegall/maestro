b9bc85378d4400ffc42cad917977acb4e2d3e918
isaacgaretmia <isaacgaretmia@gmail.com>
2013-01-10 17:29:09 -0500

    Wrong variable name.

--- maestro/src/CommHandler.cpp
+++ maestro/src/CommHandler.cpp
@@ -29,7 +29,7 @@ using namespace std;
 			this->currMessage = canMessage;
 
 		}
-		if (NewData == this->orOutPort->read(huboCmd)){
+		if (NewData == this->orPort->read(huboCmd)){
 			this->currCmd = huboCmd;
 		}
 	}
