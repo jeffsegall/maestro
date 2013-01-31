d194922290506202466fe2e04d2d25d9a8ec370c
isaacgaretmia <isaacgaretmia@gmail.com>
2013-01-31 17:11:03 -0500

    Gunna see if that breaks.

--- maestro/src/RobotControl.cpp
+++ maestro/src/RobotControl.cpp
@@ -297,7 +297,8 @@ vector<float> trajectoryValues(string path){
     	if (!this->state->getBoards().empty()) {
 			for (map<boardNum, MotorBoard*>::iterator it = this->state->getBoards().begin(); it != this->state->getBoards().end(); it++){
 				try{
-					this->outputQueue->push(buildCanMessage(it->second->sendPositionReference()));
+					//this->outputQueue->push(buildCanMessage(it->second->sendPositionReference()));
+					it->second->sendPositionReference();
 				}
 				catch(...){
 					tempOutput << "CAUGHT AN EXCEPTION." << endl;
