e6d809088f2e1ac89255a448f458c0995e330159
isaacgaretmia <isaacgaretmia@gmail.com>
2013-01-31 15:58:39 -0500

    More debugging, you say? O.o

--- maestro/src/RobotControl.cpp
+++ maestro/src/RobotControl.cpp
@@ -295,6 +295,7 @@ vector<float> trajectoryValues(string path){
 
     	if (!this->state->getBoards().empty()) {
 			for (map<boardNum, MotorBoard*>::iterator it = this->state->getBoards().begin(); it != this->state->getBoards().end(); it++){
+				std::cout << "generating message for board num :" << boardNum << std::endl;
 				this->outputQueue->push(buildCanMessage(it->second->sendPositionReference()));
 			}
     	}
