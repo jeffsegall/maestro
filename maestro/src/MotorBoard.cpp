920a72d9c89c5372a0275fef9549c6305f145fc6
isaacgaretmia <isaacgaretmia@gmail.com>
2013-02-01 16:24:53 -0500

    Including fix for 1 motor boards.

--- maestro/src/MotorBoard.cpp
+++ maestro/src/MotorBoard.cpp
@@ -813,6 +813,8 @@ canMsg* MotorBoard::sendPositionReference(vector<int> REF, int MAX_STEP, int MIN
 
 canMsg* MotorBoard::sendPositionReference(int MAX_STEP, int MIN_STEP){
 	switch (channels){
+	case 1:
+		return new canMsg(this->BNO, TX_REF, (cmdType)2, motors[0]->interpolate(MAX_STEP, MIN_STEP), 0, 0, 0, 0, 0, 0, 0);
 	case 2:
 		return new canMsg(this->BNO, TX_REF, (cmdType)2, motors[0]->interpolate(MAX_STEP, MIN_STEP),
 														motors[1]->interpolate(MAX_STEP, MIN_STEP), 0, 0, 0, 0, 0, 0);
