603f2682909d0daf63f296346ffd25ef4c98bf32
isaacgaretmia <isaacgaretmia@gmail.com>
2013-02-19 14:31:33 -0500

    Reverting change. Hehe.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/ros/fuerte/setup.sh
-rosmake maestro && roslaunch maestro/launch/jaemi_hubo.launch.xml && roslaunch hubo_ros_visualization/launch/hubo_feedback.launch
+rosmake maestro && roslaunch maestro/launch/jaemi_hubo.launch.xml
