803930da45d4d51ce20ddb7df588adee25b920b5
isaacgaretmia <isaacgaretmia@gmail.com>
2013-02-19 14:29:02 -0500

    Updating run script?

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/ros/fuerte/setup.sh
-rosmake maestro && roslaunch maestro/launch/jaemi_hubo.launch.xml
+rosmake maestro && roslaunch maestro/launch/jaemi_hubo.launch.xml && roslaunch hubo_ros_visualization/launch/hubo_feedback.launch
