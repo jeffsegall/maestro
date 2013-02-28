e2225068ed3fba448e0dc577efddeb303c926fcf
TESTUSER <test@ubuntu.(none)>
2013-02-28 15:06:24 -0500

    Modified install script. Will now test. Hopefully will have enough space.

--- install-fuerte.sh
+++ install-fuerte.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 # This script installs ROS Fuerte with the Orocos Toolchain and
 # openRAVE stacks.
+#ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 apt-get update
 apt-get install -y mercurial
 apt-get install -y ros-fuerte-desktop-full
+echo "source /opt/ros/fuerte/setup.bash" >> ~/.bashrc
+. ~/.bashrc
+apt-get install python-rosinstall python-rosdep
 cd /opt/ros/fuerte/stacks/
-mkdir workspace
-export ROS_WORKSPACE="/opt/ros/fuerte/stacks/workspace"
-cd $ROS_WORKSPACE
-source /opt/ros/fuerte/setup.sh
-rosws init . /opt/ros/fuerte
-roscd
-rosws set -y orocos/rtt_ros_integration --git http://git.mech.kuleuven.be/robotics/rtt_ros_integration.git
-rosws set -y orocos/rtt_ros_comm --git http://git.mech.kuleuven.be/robotics/rtt_ros_comm.git
-rosws set -y orocos/rtt_common_msgs --git http://git.mech.kuleuven.be/robotics/rtt_common_msgs.git
-rosws set -y orocos/rtt_geometry --git http://git.mech.kuleuven.be/robotics/rtt_geometry.git
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-apt-get update
-apt-get install -y mercurial
-apt-get install -y ros-fuerte-desktop-full
-#rosdep install -y openrave_planning
-#cd /opt/ros/diamondback/stacks/openrave_planning/openrave_robot_control/
-#rosmake --rosdep-yes
-#ln -sf /opt/ros/diamondback/stacks/openrave_planning/openrave/bin/openrave /usr/bin/openrave
-#ln -sf /opt/ros/diamondback/stacks/openrave_planning/openrave/bin/openrave-config /usr/bin/openrave-config
+#OROCOS
+mkdir orocos
+cd /opt/ros/fuerte/stacks/orocos
+apt-get install libreadline-dev omniorb omniidl omniorb-nameserver libomniorb4-1 libomniorb4-dev libomnithread3-dev libomnithread3c2 gccxml antlr libantlr-dev libxslt1-dev liblua5.1-0-dev ruby1.8-dev libruby1.8 rubygems1.8 
+git clone --recursive git://gitorious.org/orocos-toolchain/orocos_toolchain.git
+git clone http://git.mech.kuleuven.be/robotics/rtt_ros_integration.git
+git clone http://git.mech.kuleuven.be/robotics/rtt_ros_comm.git
+git clone http://git.mech.kuleuven.be/robotics/rtt_common_msgs.git
+git clone http://git.mech.kuleuven.be/robotics/rtt_geometry.git
+roscd orocos_toolchain
+git checkout toolchain-2.5
+git submodule init
+git submodule update
+git submodule foreach git checkout toolchain-2.5
+source env.sh
+rosmake orocos_toolchain rtt_ros_integration rtt_ros_comm rtt_common_msgs rtt_geometry
+#OPENRAVE
+cd /opt/ros/fuerte/stacks
+svn co https://jsk-ros-pkg.svn.sourceforge.net/svnroot/jsk-ros-pkg/trunk/ .
+hg clone https://kforge.ros.org/armnavigation/armnavigation
+svn co https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_controllers/branches/pr2_controllers-1.4/pr2_controllers_msgs
+svn co https://code.ros.org/svn/wg-ros-pkg/stacks/pr2_common/trunk/pr2_msgs
+cd /opt/ros/fuerte/stacks/openrave_planning/openrave_robot_control/
+rosmake
+ln -sf /opt/ros/fuerte/stacks/openrave_planning/openrave/bin/openrave /usr/bin/openrave
+ln -sf /opt/ros/fuerte/stacks/openrave_planning/openrave/bin/openrave-config /usr/bin/openrave-config
 
