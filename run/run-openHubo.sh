bce9d23596e913204ff8261d78d08ca81e9ea9a4
cgtester <cgtester@cgtester-ThinkPad-T500.(none)>
2013-03-21 13:36:15 -0400

    created run-openhubo.sh. Warning - it isn't finished yet. There is no install script yet for openHubo.

---
+++ run/run-openHubo.sh
@@ -0,0 +1,15 @@
+ource /opt/ros/fuerte/setup.bash
+
+xterm -e "hubo-ach virtual" &
+sleep 7
+xterm -e ./run.sh &
+sleep 1
+
+
+
+roscd hubo_ros
+echo `pwd`
+xterm -e ./run-feedback.sh &
+sleep 1
+xterm -e ./run-feedforward.sh &
+sleep 1
