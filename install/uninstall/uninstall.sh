1e6fde7162791b22a9adb49b84c7fe716831e1d3
TESTUSER <test@ubuntu.(none)>
2013-04-04 12:39:11 -0400

    Added removal of orocos to uninstall script.

--- install/uninstall/uninstall.sh
+++ install/uninstall/uninstall.sh
@@ -17,7 +17,7 @@ echo ""
 echo "Would you like to remove Orocos Toolkit - ROS?"
 select yn in "Yes" "No"; do
         case $yn in
-                Yes ) echo "Removing Orocos Toolkit - ROS..."; sudo apt-get purge libreadline-dev omniorb omniidl omniorb-nameserver libomniorb4-1 libomniorb4-dev libomnithread3-dev libomnithread3c2 gccxml antlr libantlr-dev libxslt1-dev liblua5.1-0-dev ruby1.8-dev libruby1.8 rubygems1.8 libboost-dev; break;;
+                Yes ) echo "Removing Orocos Toolkit - ROS..."; sudo apt-get purge libreadline-dev omniorb omniidl omniorb-nameserver libomniorb4-1 libomniorb4-dev libomnithread3-dev libomnithread3c2 gccxml antlr libantlr-dev libxslt1-dev liblua5.1-0-dev ruby1.8-dev libruby1.8 rubygems1.8 libboost-dev; sudo rm -rf /opt/ros/fuerte/stacks/orocos; break;;
                 No ) echo "Skipping removal of Orocos Toolkit - ROS..."; break;;
         esac
 done
