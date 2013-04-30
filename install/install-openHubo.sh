2337624130fabd7fe33abd8a60b30c383db1a646
cgtester <cgtester@cgtester-ThinkPad-T500.(none)>
2013-04-30 13:04:37 -0400

    modified run and installation of OpenHUBO.

--- install/install-openHubo.sh
+++ install/install-openHubo.sh
@@ -3,9 +3,25 @@ echo "OpenHUBO Maestro installation Script"
 echo "Version 1.0"
 echo ""
 
-installDir=~/openHubo
-mkdir $installDir
-cd $installDir/../
+if [[ $# > 0 && $1 == "-y" ]]; then
+	QUIET=true
+fi
+
+if [[ -z "$QUIET" ]]; then
+	read -p "Please enter installation directory: " installDir
+else
+	installDir=~/
+fi
+
+if [[ ! -d "$installDir" ]]; then
+	mkdir $installDir
+	if [[ ! -d "$installDir" ]]; then
+		echo "Failed to create install directory."
+		exit 1
+	fi
+fi
+
+cd "$installDir"
 
 git clone https://github.com/daslrobotics/openHubo
 
@@ -13,14 +29,24 @@ cd openHubo
 
 sh -c './setup'
 
-echo ""
-echo "Would you like to add a source line to your bashrc file?"
-select yn in "Yes" "No"; do
-        case $yn in
-                Yes ) echo "Adding source command to bashrc..."; echo "source $installDir/env.sh" >> ~/.bashrc; break;;
-                No ) echo "Skipping modification of bashrc..."; break;;
-        esac
-done
-
-source $installDir/env.sh
+if [[ -z $(grep "source $installDir/openHubo/env.sh" ~/.bashrc)  ]]; then
+	if [[ -z "$QUIET" ]]; then
+		echo ""
+		echo "Would you like to add a source line to your bashrc file?"
+		select yn in "Yes" "No"; do
+        		case $yn in
+                	Yes )
+				echo "Adding source command to bashrc..."; 
+				echo "source $installDir/env.sh" >> ~/.bashrc; 
+				break;;
+                	No ) 
+				echo "Skipping modification of bashrc..."; 
+				break;;
+        		esac
+		done
+	else
+		echo "source $installDir/env.sh" >> ~/.bashrc;
+	fi
+fi
+source $installDir/openHubo/env.sh
 
--- run/run-openHubo.sh
+++ run/run-openHubo.sh
@@ -1,33 +1,56 @@
+#!/bin/bash
+
 
 echo "Maestro OpenHUBO Robot Control Run Script"
-if [ $# -lt 1 ]
+if [[ $# -lt 1 && ! "$HUBO_ACH_RUNNING" ]]
 then
 	echo "Usage : $0 <Mode>"
 	echo "<Mode>:"
 	echo "	virtual : Runs Hubo-Ach in Virtual (No Hardware) Mode"
-	echo "	sim	: Runs Hubo-Ach in Simulation (Hardware) Mode"
+	echo "	sim	: Runs Hubo-Ach in Simulation (Software) Mode"
+	echo "	sim-real: Runs Hubo-Ach in Simulation (Hardware) Mode"
 	echo "	real	: Runs Hubo-Ach in Robot Comm (Hardware) Mode"
 	exit
 fi
 
 source /opt/ros/fuerte/setup.bash 
+SKIP_OPEN_HUBO=""
+HUBO_ACH_RUNNING="$(pgrep hubo-daemon)"
+
+if [ "$HUBO_ACH_RUNNING" ]; then
+	echo "Hubo-Ach daemon is already running. Terminating..."
+	hubo-ach killall &> /dev/null
+	echo "Hubo-Ach daemon terminated."
+fi
 
 case $1 in
-	virtual ) xterm -e "hubo-ach virtual" &;;
-	sim ) xterm -e "hubo-ach sim openhubo nodynamics" &;;
-	real ) xterm -e "hubo-ach start" &;;
+	virtual ) 
+		xterm -e "hubo-ach virtual" &;;
+	sim ) 
+		SKIP_OPEN_HUBO=yes;
+		export PYTHONPATH="$PYTHONPATH:/usr/lib/python2.7/dist-packages" 
+		xterm -e "hubo-ach sim openhubo physics" &;;
+	sim-real ) 
+		SKIP_OPEN_HUBO=yes; 
+		export PYTHONPATH="$PYTHONPATH:/usr/lib/python2.7/dist-packages" 
+		xterm -e "hubo-ach sim openhubo nodynamics" &;;
+	real ) 
+		xterm -e "hubo-ach start" &;;
 esac
 
 sleep 3
 xterm -e ./run.sh &
 sleep 1
 
-cd $OPENHUBO_DIR/examples
-xterm -e python -i achread.py &
-sleep 1
+if [ -z "$SKIP_OPEN_HUBO" ]; then
+	source "$OPENHUBO_DIR/env.sh"
+	cd "$OPENHUBO_DIR"/examples
+	xterm -e python -i achread.py &
+	sleep 1
+fi
 
 roscd hubo_ros
-echo `pwd`
+
 xterm -e ./run-feedback.sh &
 sleep 1
 xterm -e ./run-feedforward.sh &
