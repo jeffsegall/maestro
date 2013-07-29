6a8432929a463167169032ca827099e3eefb5e73
Ryan <ryoung@ryoung-ThinkPad-Edge-E530.(none)>
2013-07-29 12:02:36 -0400

    fixed the problem I caused

--- hubomsg/msg_gen/lisp/_package_HuboCmd.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package hubomsg-msg)
-(cl:export '(JOINT-VAL
-          JOINT
-          ANGLE-VAL
-          ANGLE
-          MSG-VAL
-          MSG
-))
--- hubomsg/msg_gen/lisp/_package_HuboJointCommand.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package hubomsg-msg)
-(cl:export '(NAME-VAL
-          NAME
-          POSITION-VAL
-          POSITION
-          VELOCITY-VAL
-          VELOCITY
-))
