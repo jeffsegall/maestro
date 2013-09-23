3c2e5a7f1b7090e1775525060b0d6efa4eea2c29
cgtester <cgtester@cgtester-ThinkPad-T500.(none)>
2013-09-23 15:37:24 -0400

    Fixing hubo ach install file.

--- install/install-hubo-ach.sh
+++ install/install-hubo-ach.sh
@@ -44,12 +44,47 @@ if [[ $? != 0 ]]; then exit $BLACKLIST_VIOLATED; fi
 check blacklist file "$BLACKLISTED_FILES"
 if [[ $? != 0 ]]; then exit $BLACKLIST_VIOLATED; fi
 
+if [[ $# > 0 && $1 == "-y" ]]; then
+	QUIET=true
+fi
+
+if [[ -z "$QUIET" ]]; then
+	read -p "Please enter installation directory (No trailing '/' Please): " installDir
+else
+	installDir=~/
+fi
+
+if [[ ! -d "$installDir" ]]; then
+	mkdir $installDir
+	if [[ ! -d "$installDir" ]]; then
+		echo "Failed to create install directory."
+		exit 1
+	fi
+fi
+
+cd "$installDir"
+
+git clone https://github.com/isaacgaretmia/hubo-ach
+
+cd hubo-ach
+
+git checkout develop
+
+apt-get install autoconf automake libtool autoconf-archive
+
+autoreconf -i
+
+./configure
+
+make
+
+make install
 
 #Hubo-Ach
-add-apt-repository "deb http://code.golems.org/ubuntu precise golems.org"
-add-apt-repository "deb http://www.repo.danlofaro.com/release precise main"
-apt-get update
-apt-get install -y --force-yes --no-remove libach1 libach-dev ach-utils hubo-ach hubo-ach-dev
+#add-apt-repository "deb http://code.golems.org/ubuntu precise golems.org"
+#add-apt-repository "deb http://www.repo.danlofaro.com/release precise main"
+#apt-get update
+#apt-get install -y --force-yes --no-remove libach1 libach-dev ach-utils hubo-ach-dev
 
 source /opt/ros/fuerte/setup.bash
 
