30301a971b463b048d9ca3ab3f98367f3bdd1d22
Ryan Young <ry525525@gmail.com>
2013-09-24 13:29:43 -0400

    Modified the sound demo to be proportional

--- maestro/pyScripts/soundDemo.py
+++ maestro/pyScripts/soundDemo.py
@@ -9,26 +9,32 @@ import audioop
 from hubomsg.msg import PythonMessage
 
 if __name__ == '__main__':
+	#Initialize and set the properties of PCM object
 	card = 'default'
 	audioInput = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, card)
 	audioInput.setchannels(2)
 	audioInput.setrate(44100)
 	audioInput.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 	audioInput.setperiodsize(160)
+	#Initialize ros node and get a publisher from it
 	rospy.init_node("Noise_listener")
 	pub = rospy.Publisher("Maestro/Control", PythonMessage)
 	try:
+		#Start an infite loop that gets and analyzes audio data
 		while True:
 			l, data = audioInput.read()
 			if l:
 				maxVal = audioop.max(data, 2)
-				if maxVal > 1000:
-					pub.publish("RSP", "position", "-1.0", "")
-				elif maxVal < 100:
-					pub.publish("RSP", "position", "0.0", "")
-				time.sleep(.001)
+				# check maxVal vs two thresholds 
+				if maxVal > 500: 
+					position =(-3.14 * maxVal/1000.0)
+					if(position < -3.14):
+						position = -3.14
+					pub.publish("RSP", "position", str(position), "")
+				elif maxVal < 70:
+					position = 0
+					pub.publish("RSP", "position", str(position), "")
+				time.sleep(.001) #audio refresh rate
 	except KeyboardInterrupt :
-		sys.exit()
-	except : 
-		pass
-		
+		sys.exit() #TODO make it actually exit
+			
