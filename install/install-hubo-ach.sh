ba5a3d8ff5770ca713afd57a429432b9ae9ae702
cgtester <cgtester@cgtester-ThinkPad-T500.(none)>
2013-09-24 10:51:56 -0400

    Fixing first time install hubo-ach... hopefully.

--- install/install-hubo-ach.sh
+++ install/install-hubo-ach.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
-set -e
+#
+# This script installs Hubo-Ach and the Hubo-Ach-Ros interface for use with
+# various ROS stacks. Additionally, the hubo-ach-ros-visualization package
+# will be installed and configured.
+#
+# Options: None
+#
+# Dependencies:
+#	<Maestro Install Dir>/maestro/utils.sh
+#
+# Blacklist: None
+#
+# Author: Solis Knight
+# Date: July 2013
+
+# Change directory to the script's directory.
+if [[ `echo "$0" | grep "/" | wc -l` > 0 ]]; then
+    cd ${0%/*}
+fi
+
+# Source environment checking functions.
+source ../maestro/utils.sh
+
+# Stop execution on any significant error.
+#set -e
+
 echo "Hubo-ACH - Maestro installation script"
-echo "Version 1.0"
+echo "Version $VERSION"
 echo ""
 
-#Hubo-Ach
-#add-apt-repository "deb http://code.golems.org/ubuntu precise golems.org"
-#add-apt-repository "deb http://www.repo.danlofaro.com/release precise main"
-#apt-get update
-#apt-get install -y --force-yes --no-remove libach1 libach-dev ach-utils hubo-ach hubo-ach-dev
+DEPENDENCY_DIRS="/opt /opt/ros /opt/ros/fuerte /opt/ros/fuerte/stacks"
+DEPENDENCY_FILES="/opt/ros/fuerte/setup.bash"
+BLACKLISTED_DIRS="/opt/ros/fuerte/stacks/hubo-ach-ros
+/opt/ros/fuerte/stacks/hubo-ach-ros-visualization"
+BLACKLISTED_FILES=""
+
+check dependency dir "$DEPENDENCY_DIRS"
+if [[ $? != 0 ]]; then exit $NOT_FOUND; fi
+check dependency file "$DEPENDENCY_FILES"
+if [[ $? != 0 ]]; then exit $NOT_FOUND; fi
+check blacklist dir "$BLACKLISTED_DIRS"
+if [[ $? != 0 ]]; then exit $BLACKLIST_VIOLATED; fi
+check blacklist file "$BLACKLISTED_FILES"
+if [[ $? != 0 ]]; then exit $BLACKLIST_VIOLATED; fi
 
 if [[ $# > 0 && $1 == "-y" ]]; then
 	QUIET=true
@@ -36,7 +70,9 @@ cd hubo-ach
 
 git checkout develop
 
-apt-get install autoconf automake libtool autoconf-archive
+add-apt-repository "deb http://code.golems.org/ubuntu precise golems.org"
+apt-get update
+apt-get install libach1 libach-dev ach-utils autoconf automake libtool autoconf-archive
 
 autoreconf -i
 
@@ -45,6 +81,12 @@ autoreconf -i
 make
 
 make install
+
+#Hubo-Ach
+#add-apt-repository "deb http://code.golems.org/ubuntu precise golems.org"
+#add-apt-repository "deb http://www.repo.danlofaro.com/release precise main"
+#apt-get update
+#apt-get install -y --force-yes --no-remove libach1 libach-dev ach-utils hubo-ach-dev
 
 source /opt/ros/fuerte/setup.bash
 
